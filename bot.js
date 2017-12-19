38d4196945e7d7de20f3e3544e417a956140c458
BuildTools <unconfigured@null.spigotmc.org>
2017-12-19 02:04:15 +0100

    Added the sqlite for the database we will be using. Also added new functions and added alot of code

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
-const client = new Discord.client;
-const auth = require('./auth.json');
+const client = new Discord.Client();
+const sql = require("sqlite");
+const auth = require("./auth.json");
+sql.open("./submissions.sqlite");
 
 client.on("ready", () => {
   client.user.setGame("#help for info");
@@ -13,74 +15,113 @@ client.on("message", (message) => {
   const args = message.content.slice(auth.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   const commandArgs = args.join(' ');
+  var applied;
 
-  if(message.channel.type === "dm" && commandArgs && ["id of the user is in the database and not signed up yet"]){
-    switch (command) {
-      case "name":
-        name(commandArgs);
-      break;
-      case "age":
-        age(commandArgs);
-      break;
-      case "timezone":
-        timezone(commandArgs);
-      break;
-      case "place":
-        place(commandArgs);
-      break;
-      case "role":
-        role(commandArgs);
-      break;
-      case "gender":
-        gender(commandArgs);
-      break;
-      case "activity":
-        activity(commandArgs);
-      break;
-      case "info":
-        info(commandArgs);
-      break;
-      case "reason":
-        reason(commandArgs);
-      break;
-      default:
-        "Sorry, but that does not work here..."
+  if(message.channel.type === "dm"){
+    if(command === "start"){
+      sql.get(`SELECT * FROM submissions WHERE userId = "${message.author.id}"`).then(row => {
+        if(!row){
+          sql.run("INSERT INTO submissions (userId, applied, name, age, timezone, place, role, gender, activity, reason, info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NULL)", [message.author.id, "NULL"]);
+        } else {
+          return message.channel.send("Sorry, but you have already applied and we have not yet looked at your application.");
+        }
+        }).catch(() => {
+          console.error;
+          sql.run("CREATE TABLE IF NOT EXISTS submissions (userId TEXT PRIMARY KEY, applied TEXT, name TEXT, age INTEGER, timezone TEXT, place TEXT, role TEXT, gender TEXT, activity TEXT, reason TEXT, info TEXT)").then(() => {
+            sql.run("INSERT INTO submissions (userId, applied, name, age, timezone, place, role, gender, activity, reason, info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NULL)", [message.author.id, "NULL"]);
+          });
+        });
+      applied = sql.get(`SELECT applied FROM submissions WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply("AN ERROR HAS OCCURED");
+      });
+      const values = "#name #age #timezone #place #role #gender #activity #reason #info";
+      return message.channel.send(values);
+    }
+    //TODO: turn applied on when you are done with the form!
+    if(applied === null || applied === "" || applied == "NULL"){
+      switch (command) {
+        case "name":
+          name(commandArgs, message);
+        break;
+        case "age":
+          age(commandArgs, message);
+        break;
+        case "timezone":
+          timezone(commandArgs, message);
+        break;
+        case "place":
+          place(commandArgs, message);
+        break;
+        case "role":
+          role(commandArgs, message);
+        break;
+        case "gender":
+          gender(commandArgs, message);
+        break;
+        case "activity":
+          activity(commandArgs, message);
+        break;
+        case "info":
+          info(commandArgs, message);
+        break;
+        case "reason":
+          reason(commandArgs, message);
+        break;
+        default:
+          "Sorry, but that does not work here..."
+      }
     }
   }
   else {
-    if(command === "apply"){
-      return message.reply("test");
+    if(command === "apply" || command === "submission"){
+      console.log("test");
+      return message.author.sendMessage("Hello "+message.author.username+"!"+
+        "\nThanks for your interest in becoming moderator!"+
+        "\nThere are a couple of steps to take before anyone can become a moderator."+
+        "\nFirst there are some basic questions you’ll have to answer."+
+        "\nIf you get through, one of our head moderators will have a 1 on 1 chat conversation with you to see if you’ll be suited as a moderator."+
+        "\nPlease note that we do not need moderators at all time. There is definitely no reason to feel sad if you don’t make it through."+
+        "\nYou can still have a great time chatting on YouTube and Discord!"+
+        "\nTo start with the application please type #start");
     }
   }
 });
 
-function name(commandArgs) {
-
+function name(commandArgs, message) {
+  sql.run(`UPDATE submissions SET name = ${""+commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function age(commandArgs) {
-
+function age(commandArgs, message) {
+  sql.run(`UPDATE submissions SET age = ${parseInt(commandArgs)} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function timezone(commandArgs) {
-
+function timezone(commandArgs, message) {
+  sql.run(`UPDATE submissions SET timezone = ${commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function place(commandArgs) {
-
+function place(commandArgs, message) {
+  sql.run(`UPDATE scosubmissionsres SET place = ${commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function role(commandArgs) {
-
+function role(commandArgs, message) {
+  sql.run(`UPDATE submissions SET role = ${commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function gender(commandArgs) {
-
+function gender(commandArgs, message) {
+  sql.run(`UPDATE submissions SET gender = ${commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function activity(commandArgs) {
-
+function activity(commandArgs, message) {
+  sql.run(`UPDATE submissions SET activity = ${commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function info(commandArgs) {
-
+function info(commandArgs, message) {
+  sql.run(`UPDATE submissions SET info = ${commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
-function reason(commandArgs) {
-
+function reason(commandArgs, message) {
+  sql.run(`UPDATE submissions SET reason = ${commandArgs} WHERE userId = ${message.author.id}`);
+  return message.channel.send("Value set.");
 }
 
-
-client.login(auth.secret);
+client.login(auth.token);
